11455cb3867bc5c64ee7a365d5e3fc646568dcab
Ryan Blue <blue@apache.org>
2019-10-25 14:26:41 -0700

    Fix identifier validation preventing metadata tables from loading.

--- metacat/src/main/java/com/netflix/iceberg/metacat/MetacatIcebergCatalog.java
+++ metacat/src/main/java/com/netflix/iceberg/metacat/MetacatIcebergCatalog.java
@@ -5,8 +5,11 @@ import com.netflix.iceberg.KSGatewayListener;
 import com.netflix.metacat.client.Client;
 import com.netflix.metacat.common.dto.DatabaseDto;
 import com.netflix.metacat.shaded.feign.Retryer;
+import java.util.Map;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.BaseMetastoreCatalog;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Schema;
 import org.apache.iceberg.TableOperations;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.exceptions.NoSuchTableException;
@@ -50,18 +53,13 @@ public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
 
   @Override
   protected TableOperations newTableOps(TableIdentifier tableIdentifier) {
-    validateTableIdentifier(tableIdentifier, true);
-
     String catalog = tableIdentifier.namespace().level(0);
     String database = tableIdentifier.namespace().level(1);
-
-    return new MetacatClientOps(conf, newClient(), catalog, database, tableIdentifier.toString());
+    return new MetacatClientOps(conf, newClient(), catalog, database, tableIdentifier.name());
   }
 
   @Override
   protected String defaultWarehouseLocation(TableIdentifier tableIdentifier) {
-    validateTableIdentifier(tableIdentifier, false);
-
     String catalog = tableIdentifier.namespace().level(0);
     String database = tableIdentifier.namespace().level(1);
 
@@ -79,8 +77,26 @@ public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
   }
 
   @Override
+  public org.apache.iceberg.Table createTable(
+      TableIdentifier identifier, Schema schema, PartitionSpec spec, String location, Map<String, String> properties) {
+    Preconditions.checkArgument(isValidIdentifier(identifier),
+        "Identifiers must be catalog.database.table: %s", identifier);
+    return super.createTable(identifier, schema, spec, location, properties);
+  }
+
+  @Override
+  public org.apache.iceberg.Table loadTable(TableIdentifier identifier) {
+    if (!isValidIdentifier(identifier)) {
+      throw new NoSuchTableException("Identifiers must be catalog.database.table: %s", identifier);
+    }
+    return super.loadTable(identifier);
+  }
+
+  @Override
   public boolean dropTable(TableIdentifier tableIdentifier, boolean purge) {
-    validateTableIdentifier(tableIdentifier, true);
+    if (!isValidIdentifier(tableIdentifier)) {
+      throw new NoSuchTableException("Identifiers must be catalog.database.table: %s", tableIdentifier);
+    }
 
     if (purge) {
       // TODO: ensure purge isn't set by default
@@ -97,8 +113,10 @@ public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
 
   @Override
   public void renameTable(TableIdentifier from, TableIdentifier to) {
-    validateTableIdentifier(from, true);
-    validateTableIdentifier(to, false);
+    if (!isValidIdentifier(from)) {
+      throw new NoSuchTableException("Identifiers must be catalog.database.table: %s", from);
+    }
+    Preconditions.checkArgument(isValidIdentifier(to), "Identifiers must be catalog.database.table: %s", to);
 
     String fromCatalog = from.namespace().level(0);
     String toCatalog = to.namespace().level(0);
@@ -119,16 +137,8 @@ public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
     newClient().getApi().renameTable(fromCatalog, fromDatabase, fromTableName, toTableName);
   }
 
-  private static void validateTableIdentifier(TableIdentifier tableIdentifier, boolean throwNoSuchTable) {
-    if (!tableIdentifier.hasNamespace() || tableIdentifier.namespace().levels().length != 2) {
-      if (throwNoSuchTable) {
-        throw new NoSuchTableException(
-            "Identifiers must be catalog.database.table: %s", tableIdentifier);
-      } else {
-        throw new IllegalArgumentException(
-            String.format("Identifiers must be catalog.database.table: %s", tableIdentifier));
-      }
-    }
+  private boolean isValidIdentifier(TableIdentifier tableIdentifier) {
+    return tableIdentifier.hasNamespace() && tableIdentifier.namespace().levels().length == 2;
   }
 
   private Client newClient() {
